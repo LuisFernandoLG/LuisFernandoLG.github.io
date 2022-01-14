a6f101c12a434b3d4a8aaa112b3228565ebc6f24
Luis Lopez <luislopez1099lg@gmail.com>
2022-01-14 10:30:42 -0700

    fix(Root): data loaded from js

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,43 @@
-import { proyects } from "./data";
+const proyects = [
+  {
+    id: 1,
+    name: "gsp cabo landing Page",
+    link: "https://gspcabo.netlify.app/",
+    thumbail: "./dist/assets/img/proyects/gspcabo.png",
+  },
+  {
+    id: 2,
+    name: "Emoji App",
+    link: "https://admiring-kalam-5cd446.netlify.app",
+    thumbail: "./dist/assets/img/proyects/emojiapp.png",
+  },
+  {
+    id: 3,
+    name: "Type And Type",
+    link: "https://typing-applg.netlify.app",
+    thumbail: "./dist/assets/img/proyects/typeandtype.png",
+  },
+  {
+    id: 4,
+    name: "Planet fact",
+    link: "https://planet-factss.netlify.app",
+    thumbail: "./dist/assets/img/proyects/planetsSite.png",
+  },
+
+  {
+    id: 5,
+    name: "Linked List",
+    link: "https://lista-enlazada.netlify.app",
+    thumbail: "./dist/assets/img/proyects/linkedList.png",
+  },
+
+  {
+    id: 6,
+    name: "GSP Form",
+    link: "https://admiring-kalam-5cd446.netlify.app",
+    thumbail: "./dist/assets/img/proyects/gspform.png",
+  },
+];
 
 const loadProyects = () => {
   let $proyectsContainer = document.querySelector(".proyects");
@@ -15,4 +54,6 @@ const loadProyects = () => {
   $proyectsContainer.innerHTML = $proyectsLoaded;
 };
 
-loadProyects();
+window.addEventListener("DOMContentLoaded", (event) => {
+  loadProyects();
+});
