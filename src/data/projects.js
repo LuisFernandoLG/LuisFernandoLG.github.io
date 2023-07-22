921f26d7645f5ae18348a96af1712d30e5d91818
LuisLópez <luislopez1099lg@gmail.com>
2023-07-21 21:56:53 -0600

    new portafolio

---
+++ src/data/projects.js
@@ -0,0 +1,10 @@
+const proyects = [
+    {
+        src: "./src/assets/images/proyects/gsp.jpeg",
+        title: "GSP landing page",
+        description: "Landing page developed for promoting the Green Scholarship Program.",
+        url: "https://pokedex-rosy.vercel.app/",
+        tecnologies: ["HTML", "CSS", "JavaScript", "API"],
+        color: "#226742 ",
+    },
+]
---
+++ src/js/app.js
@@ -0,0 +1,95 @@
+window.addEventListener("DOMContentLoaded", () => {
+    loadprojects()
+})
+
+const projects = [
+    {
+        src: "./src/assets/images/projects/practibus.png",
+        title: "Practibus",
+        description: "Practibus is an ecosystem of apps for bus for SJC. Driver , user and admin app. This is the user app.",
+        url: "https://butterflymusicprogram.com/",
+        tecnologies: ["React native", "Firebase", "MongoDB", "NodeJs"],
+        color: "#186B82",
+        inProcess: true
+    },
+    {
+        src: "./src/assets/images/projects/butterfly.jpeg",
+        title: "Butterfly Music Program",
+        description: "Website developed for the Butterfly Music Program, a music school in the Cabo San Lucas.",
+        url: "https://butterflymusicprogram.com/",
+        tecnologies: ["Wordpress", "JavaScript", "CSS"],
+        color: "#673800",
+    },
+    {
+        src: "./src/assets/images/projects/gsp.jpeg",
+        title: "GSP landing page",
+        description: "Landing page developed for promoting the Green Scholarship Program.",
+        url: "https://gspcabo.netlify.app/",
+        tecnologies: ["HTML", "CSS", "JavaScript"],
+        color: "#226742",
+    },
+
+    
+    {
+        src: "./src/assets/images/projects/linkedList.jpeg",
+        title: "Simple Linked List",
+        description: "Linked List app developed for the Data Structures course in order to learn how it workds.",
+        url: "https://lista-enlazada.netlify.app/",
+        tecnologies: ["HTML", "CSS", "JavaScript"],
+        color: "#272727",
+    },
+
+    
+    
+    {
+        src: "./src/assets/images/projects/planets.jpeg",
+        title: "Planest Facts",
+        description: "Web app developed for learning React and consuming an API.",
+        url: "https://app.netlify.com/sites/planet-factss/",
+        tecnologies: ["React", "Styledcomponents"],
+        color: "#2E1A47",
+    },
+
+    {
+        src: "./src/assets/images/projects/emoji.jpeg",
+        title: "Emoji app",
+        description: "Web app developed for learning React and animations.",
+        url: "https://admiring-kalam-5cd446.netlify.app/",
+        tecnologies: ["React", "Styledcomponents"],
+        color: "#000000",
+    }
+
+
+]
+
+const $projects = document.querySelector(".projects");
+
+const getprojectElement = ({ src, title, description, url, tecnologies, color, inProcess }) => {
+
+    const tecnologiesElement = tecnologies.map((tecnology) => {
+        return `<span class="tecnology" style="color:${color};">${tecnology}</span>`
+    }).join("")
+
+
+    return `
+    <figure class="project ${inProcess ? "project-inprocess" : ''}" style="background-image: url('${src}'); background-size: contain; &::before: linear-gradient(transparent 0%, ${color} 100%);">
+    ${ inProcess ? '<span class="inprogress-chip"><img src="/src/assets/images/gear.png"/> Work in Progress...</span>' : ''}
+    
+    
+    <div class="project__overlay" style="background: linear-gradient(transparent 0%, ${color} 100%);"></div>
+    <a class=" project__title ${inProcess ? "project__title-inprogress" : ""}" href="${url}" target="_blank"> <img src="./src/assets/icons/world.svg" alt=${title}">${title}</a>
+    <figcaption class="project__description">${description}</figcaption>
+    <div class="tecnologies">
+    ${tecnologiesElement}
+    </div>
+  </figure>
+    `
+}
+
+const loadprojects = () => {
+    const projectsElements = projects.map((project) => {
+        return getprojectElement(project)
+    }).join("")
+
+    $projects.innerHTML = projectsElements
+}
